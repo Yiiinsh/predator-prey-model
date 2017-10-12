b95a2e05bfdae45a4912dbffae58c04b8098456d
Shaohan Yin <shaohan.yin@gmail.com>
2017-10-12 20:32:07 +0100

    [DEV] LandscapeFactory create test

---
+++ core/src/test/java/org/pecc/ps/core/entity/environment/LandscapeFactoryTest.java
@@ -0,0 +1,24 @@
+package org.pecc.ps.core.entity.environment;
+
+import org.epcc.ps.core.entity.environment.Grid;
+import org.epcc.ps.core.entity.environment.GridFactory;
+import org.epcc.ps.core.entity.environment.LandscapeFactory;
+import org.epcc.ps.core.entity.environment.Terrain;
+import org.junit.Assert;
+import org.junit.Test;
+import org.pecc.ps.core.AbstractTest;
+
+/**
+ * @author shaohan.yin
+ * Created on 12/10/2017
+ */
+public class LandscapeFactoryTest extends AbstractTest {
+    @Test
+    public void testLandscapeCreate() {
+        Grid[][] grids = {
+                {GridFactory.getInstance().create(Terrain.LAND),}
+        };
+        Assert.assertNotNull(LandscapeFactory.getInstance().create(1,1, grids));
+    }
+
+}
